c238e829cb4a714f669869c801a25ba872407337
preshy <110857483+preshiy@users.noreply.github.com>
2026-04-26 19:13:30 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -13,7 +13,8 @@ app.use(cors({
   origin: [
     "http://localhost:5174",
     "http://localhost:5173",
-    "https://podcast-pipeline-backend.onrender.com"
+    "https://podcast-pipeline-backend.onrender.com",
+    "https://podcast-pipeline-six.vercel.app"
   ]
 }));
 
